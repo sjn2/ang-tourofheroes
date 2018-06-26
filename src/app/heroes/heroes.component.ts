49ae14cffdd01eb8a938a6d1e4513b44dac0fc8d
SureshSajjan <suresh.sajjan@metrobank.plc.uk>
2018-06-26 16:50:32 +0100

    dashboard changes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,12 +11,6 @@ import { HeroService} from '../hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
     // this.heroes = this.heroService.getHeroes();
     this.heroService.getHeroes()
